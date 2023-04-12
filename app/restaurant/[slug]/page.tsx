a70e16fc9e1f639c09be4ed9057adb2e250e22cc
Estera Nowak <esteranowak95@gmail.com>
2023-04-12 19:03:38 +0200

    defined page layouts and head meta

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,6 +1,4 @@
-import NavBar from "../../components/NavBar";
 import Description from "./components/Description";
-import Header from "./components/Header";
 import Images from "./components/Images";
 import Rating from "./components/Rating";
 import ReservationCard from "./components/ReservationCard";
@@ -10,11 +8,7 @@ import Title from "./components/Title";
 
 export default function RestaurantDetails(){
   return (
-<main className="bg-gray-100 min-h-screen w-screen">
-  <main className="max-w-screen-2xl m-auto bg-white">
-    <NavBar />
-    <Header />
-    <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+    <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
         <RestaurantNavBar />
         <Title />
@@ -24,10 +18,8 @@ export default function RestaurantDetails(){
         <Reviews />
       </div>
      <ReservationCard />
-    </div>
+    
+    </>
    
-  </main>
-</main>
-
   )
 }
