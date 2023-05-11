3e4e30f5a9c7253e32134754c9e6c79b715f7bf9
Estera Nowak <esteranowak95@gmail.com>
2023-05-11 07:59:52 +0200

    repaired searchbar

--- app/search/components/RestaurantCard.tsx
+++ app/search/components/RestaurantCard.tsx
@@ -1,21 +1,28 @@
 import Link from 'next/link';
 import { RestaurantCardType } from '../../page';
 import React from 'react';
+import { Cuisine, PRICE, Location } from '@prisma/client';
 
 interface Restaurant {
-  retaurant: RestaurantCardType;
+  id: number;
+  name: string;
+  main_image: string;
+  price: PRICE;
+  cuisine: Cuisine;
+  location: Location;
+  slug: string;
 }
 
-export default function RestaurantCard({}: Restaurant) {
+export default function RestaurantCard({
+  restaurant,
+}: {
+  restaurant: Restaurant;
+}) {
   return (
     <div className="border-b flex pb-5">
-      <img
-        src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-        alt=""
-        className="w-44 rounded"
-      />
+      <img src={restaurant.main_image} alt="" className="w-44 rounded" />
       <div className="pl-5">
-        <h2 className="text-3xl">Aiāna Restaurant Collective</h2>
+        <h2 className="text-3xl">{restaurant.name}</h2>
         <div className="flex items-start">
           <div className="flex mb-2">*****</div>
           <p className="ml-2 text-sm">Awesome</p>
@@ -23,12 +30,14 @@ export default function RestaurantCard({}: Restaurant) {
         <div className="mb-9">
           <div className="font-light flex text-reg">
             <p className="mr-4">$$$</p>
-            <p className="mr-4">Mexican</p>
-            <p className="mr-4">Ottawa</p>
+            <p className="mr-4">{restaurant.cuisine.name}</p>
+            <p className="mr-4">{restaurant.location.name}</p>
           </div>
         </div>
         <div className="text-red-600">
-          <Link href="/restaurant/milestones-grill">View more information</Link>
+          <Link href={`/restaurant/${restaurant.slug}`}>
+            View more information
+          </Link>
         </div>
       </div>
     </div>
